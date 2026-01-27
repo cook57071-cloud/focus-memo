83ea723d0ff49d16a6233c45b5c3b76f05de15fd
유가영 <gyullby11@gmail.com>
2026-01-27 21:17:45 +0900

    chore: Setup routing and draft memo views for list/detail by 유가영

--- memo/views.py
+++ memo/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import MemoForm
-from .models import Memo
 from django.contrib import messages
 
-@login_required  # 로그인한 사용자만 접근 가능
+from .forms import MemoForm
+from .models import Memo
+
+
 @login_required
 def memo_create(request):
     if request.method == 'POST':
@@ -14,8 +15,20 @@ def memo_create(request):
             memo.author = request.user
             memo.save()
             messages.success(request, '메모가 저장되었습니다!')
-            return redirect('memo_list')  # ✅ 수정 포인트
+            return redirect('memo_list')  # 목록으로 이동
+        else:
+            messages.error(request, '입력 내용을 확인해 주세요.')
     else:
         form = MemoForm()
 
     return render(request, 'memo/memo_form.html', {'form': form})
+@login_required
+def memo_list(request):
+    memos = Memo.objects.filter(author=request.user)
+    return render(request, 'memo/memo_list.html', {'memos': memos})
+
+
+@login_required
+def memo_detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id, author=request.user)
+    return render(request, 'memo/memo_detail.html', {'memo': memo})
