c7200ce93fdb5018a5e296bfc964b82d0278cc6d
유가영 <gyullby11@gmail.com>
2026-01-28 18:36:58 +0900

    fix: memo list/detail/delete 오류 해결 및 템플릿 동기화

--- memo/urls.py
+++ memo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.memo_create, name='memo_create'),
+    path('', views.memo_list, name='memo_list'),  # /memo/
+    path('create/', views.memo_create, name='memo_create'),  # /memo/create/
+    path('<int:memo_id>/', views.memo_detail, name='memo_detail'),  # /memo/1/
+    path('<int:memo_id>/delete/', views.memo_delete, name='memo_delete'),  # /memo/1/delete/
 ]
--- memo/views.py
+++ memo/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import MemoForm
 from .models import Memo
-from django.contrib import messages
+from django.views.decorators.http import require_POST
+
+
 
 @login_required  # 로그인한 사용자만 접근 가능
 def memo_create(request):
@@ -21,3 +24,24 @@ def memo_create(request):
         form = MemoForm()
 
     return render(request, 'memo/memo_form.html', {'form': form})
+
+from django.views.decorators.http import require_POST
+
+@require_POST
+@login_required
+def memo_delete(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id, author=request.user)
+    memo.delete()
+    messages.success(request, "메모가 삭제되었습니다.")
+    return redirect('memo_list')
+
+@login_required
+def memo_list(request):
+    memos = Memo.objects.filter(author=request.user).order_by('-created_at')
+    return render(request, 'memo/memo_list.html', {'memos': memos})
+
+
+@login_required
+def memo_detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id, author=request.user)
+    return render(request, 'memo/memo_detail.html', {'memo': memo})
