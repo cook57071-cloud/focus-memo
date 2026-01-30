535d47a1b43dd26305c428143729ce6850f9ed39
박홍진 <cook57071@gmail.com>
2026-01-30 18:21:26 +0900

    feat: 메모 AI 시스템 - 카테고리 자동 분류, 키워드 추출

--- memo/admin.py
+++ memo/admin.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
-from .models import Memo, Category
+﻿from django.contrib import admin
+from .models import Memo
 
-admin.site.register(Memo)
-admin.site.register(Category)
+@admin.register(Memo)
+class MemoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'confidence', 'created_at']
+    list_filter = ['category', 'created_at']
+    search_fields = ['title', 'content', 'keywords']
